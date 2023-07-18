db2c56f17f1fe184541784a751f076d640b78390
Gideon Bature <infoaboutgideon@gmail.com>
2023-07-18 07:43:59 +0100

    the find path function

---
+++ shell_test/find_path.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char* findExecutableInPath(const char* executableName) {
+    char* path = getenv("PATH");
+    if (path == NULL) {
+        return NULL;
+    }
+
+    char* pathCopy = strdup(path);
+    char* directory = strtok(pathCopy, ":");
+
+    while (directory != NULL) {
+        char fullPath[1024];
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", directory, executableName);
+        
+        if (access(fullPath, X_OK) == 0) {
+            free(pathCopy);
+            return strdup(fullPath);
+        }
+
+        directory = strtok(NULL, ":");
+    }
+
+    free(pathCopy);
+    return NULL;
+}
+
+int main() {
+    const char* executableName = "my_executable";
+    char* executablePath = findExecutableInPath(executableName);
+
+    if (executablePath != NULL) {
+        printf("Executable found: %s\n", executablePath);
+        free(executablePath);
+    } else {
+        printf("Executable not found\n");
+    }
+
+    return 0;
+}
+

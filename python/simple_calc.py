600d251ce17462333dc73b9435a1172381937195
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-03 17:41:46 +0100

    add a simple calculator

---
+++ python/simple_calc.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+num1, op, num2 = input("Enter num, operator, num: ").split()
+
+num1 = int(num1)
+num2 = int(num2)
+
+if (op == '+'):
+    print(f"{num1} {op} {num2} = {num1 + num2}")
+elif (op == '-'):
+    print(f"{num1} {op} {num2} = {num1 - num2}")
+elif (op == '*'):
+    print(f"{num1} {op} {num2} = {num1 * num2}")
+elif (op == '/'):
+    print(f"{num1} {op} {num2} = {num1 / num2:.2f}")
+elif (op == '%'):
+    print(f"{num1} {op} {num2} = {num1 % num2}")
+else:
+    printf("invalid input")

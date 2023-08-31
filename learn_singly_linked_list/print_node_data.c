35c4965149eace03fb3b5ff53c4b9165a799f8f4
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-31 03:23:48 +0100

    remove malloc for ptr, since we are not creating a node

--- learn_singly_linked_list/print_node_data.c
+++ learn_singly_linked_list/print_node_data.c
@@ -5,9 +5,8 @@ void print_node_data(node_t *head)
 	node_t *ptr = NULL;
 	int i = 1;
 
-	ptr = malloc(sizeof(node_t));
 
-	if (!ptr)
+	if (!head)
 	{
 		printf("There are no nodes available\n");
 		exit(1);

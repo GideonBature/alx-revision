9017adb66a3f14c781ac9ca4ccb835ca197eaa4f
GideonBature <infoaboutgideon@gmail.com>
2023-10-25 03:41:52 +0000

    done all mandatory and add tesmperature file for advance

---
+++ mysql/SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- updates the score of Bob
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
---
+++ mysql/SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- computes the score avaerage of all records in the table
+
+SELECT AVG(score) AS average
+FROM second_table;

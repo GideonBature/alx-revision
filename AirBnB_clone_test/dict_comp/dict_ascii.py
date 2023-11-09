111cce287513ede4b16c856c98bd2c28543d60f7
GideonBature <infoaboutgideon@gmail.com>
2023-11-09 09:36:48 +0000

    dict comprehension

---
+++ AirBnB_clone_test/dict_comp/dict_ascii.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""alphabets as keys with corresponding ascii values
+"""
+dict_ascii = {chr(x): x for x in range(65, 70)}
+print(dict_ascii)
---
+++ AirBnB_clone_test/dict_comp/dict_square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""dictionary comprehension
+"""
+squares = {x: x**2 for x in range(1, 6)}
+print(squares)

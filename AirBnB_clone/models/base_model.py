da886a19c22dda1e9b7c83ddbbfcce1d7b34b3fc
GideonBature <infoaboutgideon@gmail.com>
2023-11-09 02:24:06 +0100

    add base model

--- AirBnB_clone/models/base_model.py
+++ AirBnB_clone/models/base_model.py
@@ -8,12 +8,22 @@ from datetime import datetime
 class BaseModel():
     """Defines all common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instance of objects
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                   self.__dict__[key] = value
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """prints [<class name>] (<self.id>) <self.__dict__>

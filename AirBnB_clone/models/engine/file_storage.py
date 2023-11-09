c17053b5eb737e396eeec057e533d80717900a90
GideonBature <infoaboutgideon@gmail.com>
2023-11-09 08:00:49 +0100

    add file storage module

---
+++ AirBnB_clone/models/engine/file_storage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""FileStorage class
+"""
+import json
+import os
+from .. import base_model
+
+class FileStorage():
+    """serializes instances to a JSON file
+    and deserialized JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key
+        <obj class name>.id
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+
+    def save(self):
+        """serializes __objects to the JSON file
+        (path: __file_path)
+        """
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+            json.dump(obj_dict, f)
+
+
+    def reload(self):
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists;
+        otherwise do nothing.
+        if the file doesn't exist, no exception should
+        be raised)
+        """
+        with open(self.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            self.__objects = {k: 

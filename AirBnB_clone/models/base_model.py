2eaeddc40c8eed2fafd13639cbc707267ff44c05
GideonBature <infoaboutgideon@gmail.com>
2023-11-16 03:26:04 +0100

    add files

--- AirBnB_clone/models/base_model.py
+++ AirBnB_clone/models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """The base model class
 """
 import uuid
--- AirBnB_clone/models/engine/file_storage.py
+++ AirBnB_clone/models/engine/file_storage.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """FileStorage class
 """
 import json
-import os
 from models import base_model
 
 class FileStorage():

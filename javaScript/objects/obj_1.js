1785ab55099ea7b948d29b852ee1f184fe8457c5
GideonBature <infoaboutgideon@gmail.com>
2023-11-16 03:38:33 +0100

    second js obj file

---
+++ javaScript/objects/obj_1.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const user = {};
+console.log(user);
+user.name = 'John';
+console.log(user);
+user['surname'] = 'Smith';
+console.log(user);
+user.name = 'Pete';
+console.log(user);
+delete user.name;
+console.log(user);

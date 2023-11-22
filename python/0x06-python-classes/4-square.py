2e9a6b0334d0a6ff1df0fbccbd0d5e9636391062
GideonBature <infoaboutgideon@gmail.com>
2023-11-22 20:11:27 +0000

    add getter and setter func

--- python/0x06-python-classes/4-square.py
+++ python/0x06-python-classes/4-square.py
@@ -19,7 +19,18 @@ class Square():
         self.__size = size
 
     @property
-    def 
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
 
     def area(self):
         return self.__size * self.__size

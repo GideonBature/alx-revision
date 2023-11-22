abb9b920ad85931d34be10154750813aab03d86e
GideonBature <infoaboutgideon@gmail.com>
2023-11-22 16:37:16 +0000

    add files

---
+++ python/0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""empty class
+"""
+
+class Square():
+    """square class
+    """
+    pass
---
+++ python/0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square class
+"""
+
+class Square():
+    """square class
+    """
+    def __init__(self, size):
+        """constructor
+        
+        size(private): size of square
+        """
+        self.__size = size

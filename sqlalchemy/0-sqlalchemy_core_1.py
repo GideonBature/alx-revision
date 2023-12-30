7495396f672be557fcf8538e7b85b77d10cba048
GideonBature <infoaboutgideon@gmail.com>
2023-12-30 14:43:02 +0100

    created table using sqlalchemy

---
+++ sqlalchemy/0-sqlalchemy_core_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""connecting sqlalchemy with database
+"""
+from sqlalchemy import (MetaData, Table, Column, Integer, Numeric, String, DateTime, ForeignKey, create_engine)
+from datetime import datetime
+
+metadata = MetaData()
+
+cookies = Table('cookies', metadata,
+                Column('cookie_id', Integer(), primary_key=True),
+                Column('cookie_name', String(50), index=True),
+                Column('cookie_recipe_url', String(255)),
+                Column('cookie_sku', String(55)),
+                Column('quantity', Integer()),
+                Column('unit_cost', Numeric(12, 2))
+                )
+
+users = Table('users', metadata,
+              Column('user_id', Integer(), primary_key=True),
+              Column('customer_number', Integer(), autoincrement=True),
+              Column('username', String(15), nullable=False, unique=True),
+              Column('email_address', String(255), nullable=False, unique=True),
+              Column('phone', String(20), nullable=False),
+              Column('password', String(25), nullable=False),
+              Column('created_on', DateTime(), default=datetime.now),
+              Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+              )
+
+orders = Table('orders', metadata,
+               Column('order_id', Integer(), primary_key=True),
+               Column('user_id', ForeignKey('users.user_id'))
+               )
+
+line_items = Table('line_items', metadata,
+                   Column('line_items_id', Integer(), primary_key=True),
+                   Column('order_id', ForeignKey('orders.order_id')),
+                   Column('cookie_id', ForeignKey('cookies.cookie_id')),
+                   Column('quantity', Integer()),
+                   Column('extended_cost', Numeric(12, 2))
+                   )
+
+engine = create_engine("mysql+mysqldb://root:@localhost/test", pool_recycle=3600)
+metadata.create_all(engine)
